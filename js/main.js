692e9c16eab0ff4caf2ac32c87518be438b9bf8c
pikaybh <pikabh@naver.com>
2021-10-03 17:12:57 +0900

    homepage navbar update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-let header = "<!-- Use any element to open the sidenav --><header>    <span class='nav_btn material-icons' onclick='openNav()'>menu</span></header><nav id='mySidenav' class='sidenav'>    <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>    <ul>        <span class='logo'><a class='logo_text' href='/'><span class='material-icons'>home</span> 홈페이지</a></span>        <li><a href='../beam'><span class='material-icons'>border_horizontal</span> 보</a></li>        <li><a href='#'><span class='material-icons'>border_vertical</span> 기둥</a></li>    </ul></nav><!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page --><div id='main' onclick='closeNav()'></div>";
+let header = "<!-- Use any element to open the sidenav --><header>    <span class='nav_btn material-icons' onclick='openNav()'>menu</span></header><nav id='mySidenav' class='sidenav'>    <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>&times;</a>    <ul>        <span class='logo'><a class='logo_text' href='/'><span class='material-icons'>home</span> 홈페이지</a></span>        <li><a href='/beam'><span class='material-icons'>border_horizontal</span> 보</a></li>        <li><a href='#'><span class='material-icons'>border_vertical</span> 기둥</a></li>    </ul></nav><!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page --><div id='main' onclick='closeNav()'></div>";
 let header1 = "<!-- Use any element to open the sidenav --><header><span class='logo-text'>HelloWorld!</span></header>";
 let footer = "<footer>32172651유병희 ©&nbsp<span class='copyY' id='copyY'></span></footer>";
 if (document.getElementById("header") == null) {
-    document.getElementById("header1").innerHTML = header1;
+    document.getElementById("header1").innerHTML = header;
 } else {
     document.getElementById("header").innerHTML = header;
 }
